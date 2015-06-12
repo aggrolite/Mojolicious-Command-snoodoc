9d3cea9e300d44a84596a90687b13e2f9c722e5f
Curtis Brandt <curtis@cpan.org>
2015-06-11 21:04:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Mojolicious::Command::snoodoc - Blah blah blah
+
+# SYNOPSIS
+
+    use Mojolicious::Command::snoodoc;
+
+# DESCRIPTION
+
+Mojolicious::Command::snoodoc is
+
+# AUTHOR
+
+Curtis Brandt <curtis@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2015- Curtis Brandt
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Mojolicious/Command/snoodoc.pm
@@ -0,0 +1,39 @@
+package Mojolicious::Command::snoodoc;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Command::snoodoc - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Command::snoodoc;
+
+=head1 DESCRIPTION
+
+Mojolicious::Command::snoodoc is
+
+=head1 AUTHOR
+
+Curtis Brandt E<lt>curtis@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Curtis Brandt
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
